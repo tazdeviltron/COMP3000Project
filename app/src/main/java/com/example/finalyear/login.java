0e6461d616639e9ec437a41666c501fa7699e840
tazdeviltron <149820174+tazdeviltron@users.noreply.github.com>
2025-12-16 16:56:55 +0000

    stuff

--- app/src/main/java/com/example/finalyear/login.java
+++ app/src/main/java/com/example/finalyear/login.java
@@ -27,5 +27,17 @@ import org.json.JSONObject;
 
 public class login extends AppCompatActivity {
 
+    @SuppressLint({"MissingInflatedId", "ResourceType"})
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.xml.loginpage);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
 
 }
