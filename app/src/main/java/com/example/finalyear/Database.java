963c5bdf7a1f78b28fe400e15e548a9f8064e51f
tazdeviltron <149820174+tazdeviltron@users.noreply.github.com>
2026-02-13 19:26:35 +0000

    stuff

---
+++ app/src/main/java/com/example/finalyear/Database.java
@@ -0,0 +1,5 @@
+package com.example.finalyear;
+
+public class Database {
+
+}
--- app/src/main/java/com/example/finalyear/login.java
+++ app/src/main/java/com/example/finalyear/login.java
@@ -1,31 +1,22 @@
 package com.example.finalyear;
 import android.annotation.SuppressLint;
-import android.content.SharedPreferences;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.os.AsyncTask;
 import android.os.Bundle;
 //import android.database.Cursor;
-import android.content.Intent;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import android.util.Log;
-import com.android.volley.Request;
+
 import com.android.volley.RequestQueue;
-import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 public class login extends AppCompatActivity {
+    private EditText usernameEditText, passwordEditText;
+    private RequestQueue requestQueue;
+    private Database dbHelper;
 
     @SuppressLint({"MissingInflatedId", "ResourceType"})
     @Override
@@ -33,11 +24,23 @@ public class login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.loginpage);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
 
+        usernameEditText = findViewById(R.id.editTextText);
+        passwordEditText = findViewById(R.id.editTextPassword);
+        Button loginButton = findViewById(R.id.submit);
+        Button RegButton = findViewById(R.id.newuser);
+        loginButton.setOnClickListener(view -> {
+            String username = usernameEditText.getText().toString();
+            String password = passwordEditText.getText().toString();
+        });
+        RegButton.setOnClickListener(view ->{
+            setContentView(R.layout.regpage);
+        });
+    }
 }
