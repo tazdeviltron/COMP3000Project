c794b2bac3a48d89158338cdef529e812d30e043
tazdeviltron <149820174+tazdeviltron@users.noreply.github.com>
2026-03-11 17:35:07 +0000

    stuff

--- MyHealth/app/build.gradle.kts
+++ MyHealth/app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.material)
+    implementation(libs.androidx.recyclerview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
